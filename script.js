8d60e1d38a7b7cf3df93b795a6d3dc395e96e347
speckoh <speckohart@gmail.com>
2023-01-04 10:48:12 -0500

    mvp v1.0 update

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+const gameContainer = document.querySelector(".gameContainer");
 const boardContainer = document.querySelector(".gridContainer");
 const mineSprite = "http://www.speckoh.com/images/mine.png";
+const flagSprite = "http://www.speckoh.com/images/flag.png";
 
 let boardArray = [];
 let randomArray = [];
@@ -11,6 +13,7 @@ let width = 9;
 let area = height * width;
 
 let emptyCheck = false;
+let totalRevealed = 0;
 
 let playerHasWon = false;
 let playerHasLost = false;
@@ -21,6 +24,8 @@ class Square {
         this.hasMine = false;
         this.adjacentMines = 0;
         this.revealed = false;
+        this.flooded = false;
+        this.flagged = false;
     }
 }
 
@@ -40,11 +45,6 @@ for (let i = 0; i < area; i++){
     for (let j = 0; j < mineArray.length; j++){
         if(boardArray[i].index === mineArray[j].index){
             boardArray[i].hasMine = true;
-            if(boardArray[i].hasMine){
-                box.innerHTML = `<div class="hidden">
-                <img id="mine" src=${mineSprite}></div>`;
-                boardContainer.appendChild(box);
-            }
         }
     }
 }
@@ -233,7 +233,7 @@ function CheckEastForEmpty(board, index){
         emptyCheck = true;
     }
 }
-function CheckSouthForEmpty(board, index, element){
+function CheckSouthForEmpty(board, index){
     if(CheckSouth(board,index).adjacentMines === 0 && !CheckSouth(board,index).revealed){
         let element = document.getElementById(CheckSouth(board,index).index);
         element.removeChild(element.firstChild);
@@ -281,15 +281,237 @@ function CheckSouthWestForEmpty(board, index){
         emptyCheck = true;
     }
 }
+//############
+//Check For Adjacent Numbers
+//############
+function CheckNorthForNumber(board, index){
+    if(CheckNorth(board,index).adjacentMines > 0 && !CheckNorth(board,index).revealed){
+        let element = document.getElementById(CheckNorth(board,index).index);
+        element.removeChild(element.firstChild);
+        CheckNorth(board,index).revealed = true;
+    }
+}
+function CheckEastForNumber(board, index){
+    if(CheckEast(board,index).adjacentMines > 0 && !CheckEast(board,index).revealed){
+        let element = document.getElementById(CheckEast(board,index).index);
+        element.removeChild(element.firstChild);
+        CheckEast(board,index).revealed = true;
+    }
+}
+function CheckSouthForNumber(board, index){
+    if(CheckSouth(board,index).adjacentMines > 0 && !CheckSouth(board,index).revealed){
+        let element = document.getElementById(CheckSouth(board,index).index);
+        element.removeChild(element.firstChild);
+        CheckSouth(board,index).revealed = true;
+    }
+}
+function CheckWestForNumber(board, index){
+    if(CheckWest(board,index).adjacentMines > 0 && !CheckWest(board,index).revealed){
+        let element = document.getElementById(CheckWest(board,index).index);
+        element.removeChild(element.firstChild);
+        CheckWest(board,index).revealed = true;
+    }
+}
+function CheckNorthWestForNumber(board, index){
+    if(CheckNorthWest(board,index).adjacentMines > 0 && !CheckNorthWest(board,index).revealed){
+        let element = document.getElementById(CheckNorthWest(board,index).index);
+        element.removeChild(element.firstChild);
+        CheckNorthWest(board,index).revealed = true;
+    }
+}
+function CheckNorthEastForNumber(board, index){
+    if(CheckNorthEast(board,index).adjacentMines > 0 && !CheckNorthEast(board,index).revealed){
+        let element = document.getElementById(CheckNorthEast(board,index).index);
+        element.removeChild(element.firstChild);
+        CheckNorthEast(board,index).revealed = true;
+    }
+}
+function CheckSouthEastForNumber(board, index){
+    if(CheckSouthEast(board,index).adjacentMines > 0 && !CheckSouthEast(board,index).revealed){
+        let element = document.getElementById(CheckSouthEast(board,index).index);
+        element.removeChild(element.firstChild);
+        CheckSouthEast(board,index).revealed = true;
+    }
+}
+function CheckSouthWestForNumber(board, index){
+    if(CheckSouthWest(board,index).adjacentMines > 0 && !CheckSouthWest(board,index).revealed){
+        let element = document.getElementById(CheckSouthWest(board,index).index);
+        element.removeChild(element.firstChild);
+        CheckSouthWest(board,index).revealed = true;
+    }
+}
+
+function FloodEmptySquares(){
+    for (let i = 0; i < boardArray.length; i++)
+    {
+        if (boardArray[i].revealed && boardArray[i].adjacentMines === 0){
+            //Check NorthWest Corner
+            if(boardArray[i].index === 0){
+                CheckSouthForEmpty(boardArray, i);
+                CheckEastForEmpty(boardArray, i);
+                CheckSouthEastForEmpty(boardArray, i);
+            }
+            //Check NorthEast Corner
+            else if(boardArray[i].index === width - 1) {
+                CheckSouthForEmpty(boardArray, i);
+                CheckWestForEmpty(boardArray, i);
+                CheckSouthWestForEmpty(boardArray, i);
+            }
+            //Check SouthEast Corner
+            else if(boardArray[i].index === boardArray.length - 1){
+                CheckNorthForEmpty(boardArray, i);
+                CheckWestForEmpty(boardArray, i);
+                CheckNorthWestForEmpty(boardArray, i);
+            }
+            //Check SouthWest Corner
+            else if(boardArray[i].index === boardArray.length - width){
+                CheckNorthForEmpty(boardArray, i);
+                CheckEastForEmpty(boardArray, i);
+                CheckNorthEastForEmpty(boardArray, i);
+            }
+            //Check North Row
+            else if(boardArray[i].index < width) {
+                CheckSouthForEmpty(boardArray, i);
+                CheckWestForEmpty(boardArray, i);
+                CheckEastForEmpty(boardArray, i);
+                CheckSouthEastForEmpty(boardArray, i);
+                CheckSouthWestForEmpty(boardArray, i);
+            }
+            //Check East Column
+            else if(boardArray[i].index % width === width - 1) {
+                CheckNorthForEmpty(boardArray, i);
+                CheckWestForEmpty(boardArray, i);
+                CheckSouthForEmpty(boardArray, i);
+                CheckNorthWestForEmpty(boardArray, i);
+                CheckSouthWestForEmpty(boardArray, i);
+            }
+            //Check South Row
+            else if(boardArray[i].index > boardArray.length - width) {
+                CheckNorthForEmpty(boardArray, i);
+                CheckEastForEmpty(boardArray, i);
+                CheckWestForEmpty(boardArray, i);
+                CheckNorthEastForEmpty(boardArray, i);
+                CheckNorthWestForEmpty(boardArray, i);
+            }
+            //Check West Column
+            else if(boardArray[i].index % width === 0) {
+                CheckNorthForEmpty(boardArray, i);
+                CheckEastForEmpty(boardArray, i);
+                CheckSouthForEmpty(boardArray, i);
+                CheckNorthEastForEmpty(boardArray, i);
+                CheckSouthEastForEmpty(boardArray, i);
+            }
+            //Everything Middle
+            else{
+                CheckNorthForEmpty(boardArray, i);
+                CheckSouthForEmpty(boardArray, i);
+                CheckWestForEmpty(boardArray, i);
+                CheckEastForEmpty(boardArray, i);
+                CheckNorthEastForEmpty(boardArray, i);
+                CheckNorthWestForEmpty(boardArray, i);
+                CheckSouthEastForEmpty(boardArray, i);
+                CheckSouthWestForEmpty(boardArray, i);
+            }
+        }
+    }
+}
+
+function FloodNumberSquares(){
+    for (let i = 0; i < boardArray.length; i++)
+    {
+        if (boardArray[i].revealed && boardArray[i].adjacentMines === 0){
+            //Check NorthWest Corner
+            if(boardArray[i].index === 0){
+                CheckSouthForNumber(boardArray, i);
+                CheckEastForNumber(boardArray, i);
+                CheckSouthEastForNumber(boardArray, i);
+            }
+            //Check NorthEast Corner
+            else if(boardArray[i].index === width - 1) {
+                CheckSouthForNumber(boardArray, i);
+                CheckWestForNumber(boardArray, i);
+                CheckSouthWestForNumber(boardArray, i);
+            }
+            //Check SouthEast Corner
+            else if(boardArray[i].index === boardArray.length - 1){
+                CheckNorthForNumber(boardArray, i);
+                CheckWestForNumber(boardArray, i);
+                CheckNorthWestForNumber(boardArray, i);
+            }
+            //Check SouthWest Corner
+            else if(boardArray[i].index === boardArray.length - width){
+                CheckNorthForNumber(boardArray, i);
+                CheckEastForNumber(boardArray, i);
+                CheckNorthEastForNumber(boardArray, i);
+            }
+            //Check North Row
+            else if(boardArray[i].index < width) {
+                CheckSouthForNumber(boardArray, i);
+                CheckWestForNumber(boardArray, i);
+                CheckEastForNumber(boardArray, i);
+                CheckSouthEastForNumber(boardArray, i);
+                CheckSouthWestForNumber(boardArray, i);
+            }
+            //Check East Column
+            else if(boardArray[i].index % width === width - 1) {
+                CheckNorthForNumber(boardArray, i);
+                CheckWestForNumber(boardArray, i);
+                CheckSouthForNumber(boardArray, i);
+                CheckNorthWestForNumber(boardArray, i);
+                CheckSouthWestForNumber(boardArray, i);
+            }
+            //Check South Row
+            else if(boardArray[i].index > boardArray.length - width) {
+                CheckNorthForNumber(boardArray, i);
+                CheckEastForNumber(boardArray, i);
+                CheckWestForNumber(boardArray, i);
+                CheckNorthEastForNumber(boardArray, i);
+                CheckNorthWestForNumber(boardArray, i);
+            }
+            //Check West Column
+            else if(boardArray[i].index % width === 0) {
+                CheckNorthForNumber(boardArray, i);
+                CheckEastForNumber(boardArray, i);
+                CheckSouthForNumber(boardArray, i);
+                CheckNorthEastForNumber(boardArray, i);
+                CheckSouthEastForNumber(boardArray, i);
+            }
+            //Everything Middle
+            else{
+                CheckNorthForNumber(boardArray, i);
+                CheckSouthForNumber(boardArray, i);
+                CheckWestForNumber(boardArray, i);
+                CheckEastForNumber(boardArray, i);
+                CheckNorthEastForNumber(boardArray, i);
+                CheckNorthWestForNumber(boardArray, i);
+                CheckSouthEastForNumber(boardArray, i);
+                CheckSouthWestForNumber(boardArray, i);
+            }
+        }
+    }
+}
 
 //Click Events
+gameContainer.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+});
+
 const squares = document.querySelectorAll('.square');
 squares.forEach((event, index) => {
-    event.addEventListener('click', () => {
-        // console.log(boardArray[index]);
-        if(!playerHasLost){
+    event.addEventListener('mousedown', (e) => {
+        if(!playerHasLost && !playerHasWon && e.button === 0){
             if(boardArray[index].hasMine){
                 console.log("You stepped on mine, you Lose!");
+                for (let i = 0; i < boardArray.length; i++){
+                    if(boardArray[i].hasMine){
+                        let element = document.getElementById(boardArray[i].index);
+                        let mines = document.createElement("div")
+                        mines.innerHTML = `<div class="hidden">
+                        <img id="mine" src=${mineSprite}></div>`;
+                        element.removeChild(element.firstChild);
+                        element.appendChild(mines);
+                    }
+                }
                 // event.removeChild(event.firstChild);
                 playerHasLost = true;
             }
@@ -300,6 +522,7 @@ squares.forEach((event, index) => {
                     numberOfMines.innerHTML = `${boardArray[index].adjacentMines}`;
                     event.appendChild(numberOfMines);
                     boardArray[index].revealed = true;
+                    boardArray[index].flooded = true;
                     event.removeChild(event.firstChild);
                 }
                 else if(boardArray[index].adjacentMines === 0){
@@ -308,79 +531,49 @@ squares.forEach((event, index) => {
                     emptyCheck = true;
                     while(emptyCheck){
                         emptyCheck = false;
-                    for (let i = 0; i < boardArray.length; i++)
-                    {
-                        if (boardArray[i].revealed && boardArray[i].adjacentMines === 0){
-                            //Check NorthWest Corner
-                            if(boardArray[i].index === 0){
-                                CheckSouthForEmpty(boardArray, i);
-                                CheckEastForEmpty(boardArray, i);
-                                CheckSouthEastForEmpty(boardArray, i);
-                            }
-                            //Check NorthEast Corner
-                            else if(boardArray[i].index === width - 1) {
-                                CheckSouthForEmpty(boardArray, i);
-                                CheckWestForEmpty(boardArray, i);
-                                CheckSouthWestForEmpty(boardArray, i);
-                            }
-                            //Check SouthEast Corner
-                            else if(boardArray[i].index === boardArray.length - 1){
-                                CheckNorthForEmpty(boardArray, i);
-                                CheckWestForEmpty(boardArray, i);
-                                CheckNorthWestForEmpty(boardArray, i);
-                            }
-                            //Check SouthWest Corner
-                            else if(boardArray[i].index === boardArray.length - width){
-                                CheckNorthForEmpty(boardArray, i);
-                                CheckEastForEmpty(boardArray, i);
-                                CheckNorthEastForEmpty(boardArray, i);
-                            }
-                            //Check North Row
-                            else if(boardArray[i].index < width) {
-                                CheckSouthForEmpty(boardArray, i);
-                                CheckWestForEmpty(boardArray, i);
-                                CheckEastForEmpty(boardArray, i);
-                                CheckSouthEastForEmpty(boardArray, i);
-                                CheckSouthWestForEmpty(boardArray, i);
-                            }
-                            //Check East Column
-                            else if(boardArray[i].index % width === width - 1) {
-                                CheckNorthForEmpty(boardArray, i);
-                                CheckWestForEmpty(boardArray, i);
-                                CheckSouthForEmpty(boardArray, i);
-                                CheckNorthWestForEmpty(boardArray, i);
-                                CheckSouthWestForEmpty(boardArray, i);
-                            }
-                            //Check South Row
-                            else if(boardArray[i].index > boardArray.length - width) {
-                                CheckNorthForEmpty(boardArray, i);
-                                CheckEastForEmpty(boardArray, i);
-                                CheckWestForEmpty(boardArray, i);
-                                CheckNorthEastForEmpty(boardArray, i);
-                                CheckNorthWestForEmpty(boardArray, i);
-                            }
-                            //Check West Column
-                            else if(boardArray[i].index % width === 0) {
-                                CheckNorthForEmpty(boardArray, i);
-                                CheckEastForEmpty(boardArray, i);
-                                CheckSouthForEmpty(boardArray, i);
-                                CheckNorthEastForEmpty(boardArray, i);
-                                CheckSouthEastForEmpty(boardArray, i);
-                            }
-                            //Everything Middle
-                            else{
-                                CheckNorthForEmpty(boardArray, i);
-                                CheckSouthForEmpty(boardArray, i);
-                                CheckWestForEmpty(boardArray, i);
-                                CheckEastForEmpty(boardArray, i);
-                                CheckNorthEastForEmpty(boardArray, i);
-                                CheckNorthWestForEmpty(boardArray, i);
-                                CheckSouthEastForEmpty(boardArray, i);
-                                CheckSouthWestForEmpty(boardArray, i);
-                            }
+                        FloodEmptySquares();
+                    }
+                    for (let i = 0; i < boardArray.length; i++){
+                        FloodNumberSquares();
+                    }
+                    for (let i = 0; i < boardArray.length; i++){
+                        if(boardArray[i].adjacentMines > 0 && 
+                            boardArray[i].revealed && !boardArray[i].flooded){
+                            let element = document.getElementById(boardArray[i].index);
+                            let numberOfMines = document.createElement("div")
+                            numberOfMines.setAttribute("id", "numberOfMines");
+                            numberOfMines.innerHTML = `${boardArray[i].adjacentMines}`;
+                            element.appendChild(numberOfMines);
+                            boardArray[i].flooded = true;
                         }
                     }
                 }
+                totalRevealed = 0;
+                for (let i = 0; i < boardArray.length; i++){
+                    if(!boardArray[i].hasMine && boardArray[i].revealed){
+                        totalRevealed++;
+                    }
+                    if(totalRevealed === boardArray.length - mineArray.length){
+                        console.log("You Win!");
+                        playerHasWon = true;
+                    }
+                }
+            }
+        }
+        if(!playerHasLost && !playerHasWon && e.button == 2){
+            if(!boardArray[index].revealed){
+                if(!boardArray[index].flagged){
+                    let element = document.getElementById(boardArray[index].index).firstChild;
+                    let flag = document.createElement("div")
+                    flag.setAttribute("id", "flag");
+                    flag.innerHTML = `<img src=${flagSprite}>`;
+                    element.appendChild(flag);
+                    boardArray[index].flagged = true;
+                }
+                else{
+                    let element = document.getElementById(boardArray[index].index).firstChild;
+                    element.removeChild(element.firstChild);
+                    boardArray[index].flagged = false;
                 }
             }
         }
